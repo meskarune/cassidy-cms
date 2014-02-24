32bc634970dc9f203c11335ca601f9410f8d1c36
root <root@maharani.meskarune.com>
2014-02-24 15:18:58 -0500

    updated index.php to include template functions.

--- index.php
+++ index.php
@@ -1,20 +1,13 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Quadrangle Designs</title>
-		<meta name="description" content="A graphic design studio that creates unique websites and branding for small business">
-		<meta name="viewport" content="width = device-width, initial-scale = 1./0">
-		<link rel="stylesheet" type="text/css" href="/assets/css/normalize.css">
-		<link rel="stylesheet" type="text/css" media="screen" href="/assets/css/screen.css">
-		<link rel="stylesheet" type="text/css" media="screen" href="/assets/css/magnific-popup.css">
-	</head>
-	<body>
-		<?php
-			$articles = './articles';
-			foreach (new DirectoryIterator($articles) as $article) {
-					include $article;
-			}
-		?>
+<?php
+include($_SERVER['DOCUMENT_ROOT'].'/inc/template.php');
+
+html_head();
+
+$articles = './articles';
+
+foreach (new DirectoryIterator($articles) as $article) {
+	include $article;
+}
+?>
 	</body>
 </html>
